2ba55d29a5e037bbf3b8ed19b0b4409630e117d9
prmk <priymakvlad420@gmail.com>
2024-12-10 00:29:02 +0200

    styles(home-page): header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./styles/normalize.css";
+import "./styles/global.scss";
+import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
+
 function App() {
-    return <>it is working</>;
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { Headset } from "lucide-react";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.scss";
+
+const Header: FC = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Link to={"/"} className={styles.logo}>
+                    <img src="logo.png" alt="logo" />
+                </Link>
+                <Link
+                    to={"https://t.me/budkagemovbot"}
+                    target="_blank"
+                    className={styles.support}
+                >
+                    <Headset size={22} />
+                    Поддержка
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
